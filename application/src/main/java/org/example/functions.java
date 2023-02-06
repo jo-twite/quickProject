c1593d27c45570ab47943ec703b04b9733e89d6b
jo-twite <jo-twite@hotmail.com>
2023-02-06 15:30:52 +0100

    implementation of sum method

--- application/src/main/java/org/example/functions.java
+++ application/src/main/java/org/example/functions.java
@@ -5,4 +5,12 @@ public class functions {
     public static boolean containsWord(String sentence, String word) {
         return sentence.contains(word);
     }
+
+    public static int sum(int[] tab) {
+        int sum = 0;
+        for(int i: tab) {
+            x += i;
+        }
+        return sum;
+    }
 }
