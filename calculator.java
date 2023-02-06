920e397873adc5b43a8f90210b5bc6d4c95fccdd
Antonio Marashi <antonio.marashi@inetum-realdolmen.world>
2023-02-06 15:35:28 +0100

    added calculator application

---
+++ calculator.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class calculator {
+
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+}
