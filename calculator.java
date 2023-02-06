d4dfc9db7ceedad3f97bda027a61c91095bdc28f
Antonio Marashi <antonio.marashi@inetum-realdolmen.world>
2023-02-06 15:39:10 +0100

    added division function

--- calculator.java
+++ calculator.java
@@ -10,5 +10,8 @@ public class calculator {
     public static int subtract(int a, int b) {
         return a - b;
     }
+	
+	public static double divide(int a, int b) {
+		return (double) a / b;
 
 }
